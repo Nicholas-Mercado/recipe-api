984b9d4075983ad2c8830e268277866142f8f478
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-08-23 10:07:29 -0700

    adjusted model

--- things/models.py
+++ things/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 
 class Thing(models.Model):
-    name = models.CharField(max_length=256)
+    recipe = models.CharField(max_length=256)
     owner = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True
     )
-    description = models.TextField(default="", null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.recipe
