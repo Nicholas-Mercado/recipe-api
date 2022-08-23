41bc2c416930890c973dea1b1161e507c8df340d
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-08-23 09:56:52 -0700

    replaced things

--- things/serializers.py
+++ things/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Thing
 
 
-class ThingSerializer(serializers.ModelSerializer):
+class Recipeserializer(serializers.ModelSerializer):
     class Meta:
         model = Thing
         fields = "__all__"
--- things/views.py
+++ things/views.py
@@ -4,15 +4,15 @@ from rest_framework.generics import (
 )
 from .models import Thing
 from .permissions import IsOwnerOrReadOnly
-from .serializers import ThingSerializer
+from .serializers import Recipeserializer
 
 
 class ThingList(ListCreateAPIView):
     queryset = Thing.objects.all()
-    serializer_class = ThingSerializer
+    serializer_class = Recipeserializer
 
 
 class ThingDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsOwnerOrReadOnly,)
     queryset = Thing.objects.all()
-    serializer_class = ThingSerializer
+    serializer_class = Recipeserializer
