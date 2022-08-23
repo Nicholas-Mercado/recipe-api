9a318a211bfd5cfa14a3266927e988e7fdef76b3
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-08-23 12:15:05 -0700

    fixed migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
+
+    """ Creates a recipe model with recipe_name, url, and linked to ingredients"""
     recipe = models.CharField(max_length=256)
-    url = models.URLField(max_length=200, null=True)
+    url = models.URLField(max_length=200, null=True,blank=True)
+    ingredient = models.ManyToManyField(Ingredient)
     owner = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True
     )
