47a14553a5c7aa8825f7317f828223d6a352c09f
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-08-23 12:34:28 -0700

    added amount and update fields

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=30)
+    amount = models.IntegerField(blank=True, null=True)
     def __str__(self):
         return self.name
 
@@ -11,6 +12,8 @@ class Recipe(models.Model):
 
     """ Creates a recipe model with recipe_name, url, and linked to ingredients"""
     recipe = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
     url = models.URLField(max_length=200, null=True,blank=True)
     ingredient = models.ManyToManyField(Ingredient)
     owner = models.ForeignKey(
