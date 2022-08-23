6e5519f9519f329bc163549ef14b2e61f61b4121
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-08-23 11:23:16 -0700

    adjusted model

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Recipe(models.Model):
     recipe = models.CharField(max_length=256)
+    url = models.URLField(max_length=200, null=True)
     owner = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, null=True, blank=True
     )
