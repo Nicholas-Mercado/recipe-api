22f00650d5a7eb587939f26f4a9a4950b04c8ae0
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-08-24 15:52:21 -0700

    added units to ingrients

--- recipes/models.py
+++ recipes/models.py
@@ -1,18 +1,21 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from .validators import validate_unit_of_measure
+from .validators import validate_unit_of_measure, MEASUREMENT_CHOICES
+
+
 
 class Ingredient(models.Model):
+    """ Creates a ingredient model with name, amount, and units"""
     name = models.CharField(max_length=30)
     amount = models.IntegerField(blank=True, null=True)
-    unit = models.CharField(max_length=50, validators=[validate_unit_of_measure], blank=True, null=True)
+    unit = models.CharField(max_length=50, validators=[validate_unit_of_measure], choices=MEASUREMENT_CHOICES, blank=True, null=True)
     def __str__(self):
-        return self.name
+        return f"{self.amount}, {self.unit}, {self.name}"
 
 
 class Recipe(models.Model):
 
-    """ Creates a recipe model with recipe_name, url, and linked to ingredients"""
+    """ Creates a recipe model with name, url, description, and linked to ingredients"""
     recipe = models.CharField(max_length=256)
     description = models.TextField(blank=True, null=True)
     updated = models.DateTimeField(auto_now=True)
